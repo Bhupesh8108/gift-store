a88b30d1e06eb957112e9c7737c245f26e04fa20
bhupesh8108 <bhupeshdawadi12345@gmail.com>
2023-04-20 14:19:20 +0545

    css for admin

--- gifts/settings.py
+++ gifts/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-4++&(bl3ute-oj(k-^l$jljfto9_*k&)h(&=7b-=lxp2*h@#%=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['giftwan.pythonanywhere.com']
 
 
 # Application definition
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'product/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staicfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
